97b08284485f8b10cf3449763b73aa1d171019a4
Alexis Bozel <alexisbozelpro@gmail.com>
2024-12-12 14:23:55 +0100

    fix bug

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 const express = require("express")
 const requestRoute = require("./routes/requestRoute")
-const notificationRoute = require("./routes/notificationRoute")
 const port = process.env.PORT || 5000
 const cors = require('cors');
 
